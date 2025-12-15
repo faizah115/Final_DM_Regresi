678d03bea8372abdaae7a0cf33796a93329b729a
faizah115 <60900123037@uin-alauddin.ac.id>
2025-12-15 21:06:38 +0800

    perbaik

--- final_dm_faizah.py
+++ final_dm_faizah.py
@@ -10,6 +10,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
